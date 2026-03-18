8488b8071f31649415db9303a2247f851522e635
webNekit <86223001+webNekit@users.noreply.github.com>
2026-03-19 00:09:32 +0300

    Update utils.ts

--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function extractValidationErrors(errors?: any) {
+  if (!errors) return [];
+
+  return Object.values(errors)
+    .flatMap((field: any) => field?._errors ?? []);
+}
